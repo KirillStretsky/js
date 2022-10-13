b6550371e5538e8301e0019ab08934b2e34a8f3a
Kirill <examp@gmail.com>
2022-10-13 19:36:37 +0200

    second

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
 	$(".carousel__inner").slick({
 		speed: 900,
 		prevArrow:
-			'<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+			'<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow:
-			'<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+			'<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
 		responsive: [
 			{
 				breakpoint: 992,
@@ -62,5 +62,72 @@ $(document).ready(function () {
 			$("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
 			$(".overlay, #order").fadeIn("slow");
 		});
+	});
+
+	//validator
+	function validateForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2,
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true,
+				},
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста, введите свое имя",
+					minlength: jQuery.validator.format("Введите {0} символа!"),
+				},
+				phone: "Пожалуйста, введите свой номер телефона",
+				email: {
+					required: "Пожалуйста, введите свою почту",
+					email: "Неправильно введен адрес почты",
+				},
+			},
+		});
+	}
+
+	validateForms("#consultation-form");
+	validateForms("#consulting form");
+	validateForms("#order form");
+
+	$("input[name=phone]").mask("+38(99) 999 9999");
+
+	$("form").submit(function (e) {
+		e.preventDefault();
+
+		if (!$(this).valid()) {
+			return;
+		}
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize(),
+		}).done(function () {
+			$(this).find("input").val("");
+			$("#consulting, #order").fadeOut();
+			$(".overlay #thank").fadeIn("slow");
+
+			$("form").trigger("reset");
+		});
+		return false;
+	});
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1600) {
+			$(".scrollup").fadeIn();
+		} else {
+			$(".scrollup").fadeOut();
+		}
+	});
+
+	$("a[href^='#']").click(function () {
+		const _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+		return false;
 	});
 });
